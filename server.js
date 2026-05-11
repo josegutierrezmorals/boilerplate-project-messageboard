c5fa25315bd61dbcfcb0c514dbaaeb3ad872e922
josegutierrezmorals <josegutierrezmorals@gmail.com>
2026-05-10 22:01:35 -0400

    Update server.js
    Signed-off-by: josegutierrezmorals <josegutierrezmorals@gmail.com>

--- server.js
+++ server.js
@@ -1,60 +1,51 @@
 'use strict';
 require('dotenv').config();
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
+const express    = require('express');
+const bodyParser = require('body-parser');
+const cors       = require('cors');
+const helmet     = require('helmet');
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes        = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner           = require('./test-runner');
 
 const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
-
-app.use(cors({origin: '*'})); //For FCC testing purposes only
-
+app.use(cors({ origin: '*' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Sample front-end
-app.route('/b/:board/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/board.html');
-  });
-app.route('/b/:board/:threadid')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/thread.html');
-  });
+// Security headers
+app.use(helmet.frameguard({ action: 'sameorigin' }));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
 
-//Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-//For FCC testing purposes
-fccTestingRoutes(app);
-
-//Routing for API 
-apiRoutes(app);
-
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+app.route('/b/:board/').get(function (req, res) {
+  res.sendFile(process.cwd() + '/views/board.html');
+});
+app.route('/b/:board/:threadid').get(function (req, res) {
+  res.sendFile(process.cwd() + '/views/thread.html');
+});
+app.route('/').get(function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-//Start our server and tests!
+fccTestingRoutes(app);
+apiRoutes(app);
+
+app.use(function (req, res, next) {
+  res.status(404).type('text').send('Not Found');
+});
+
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
     setTimeout(function () {
       try {
         runner.run();
-      } catch(e) {
+      } catch (e) {
         console.log('Tests are not valid:');
         console.error(e);
       }
@@ -62,4 +53,4 @@ const listener = app.listen(process.env.PORT || 3000, function () {
   }
 });
 
-module.exports = app; //for testing
+module.exports = app;
